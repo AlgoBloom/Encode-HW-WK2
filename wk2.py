46469ef9d709c1390fe5318f0d5417249928d3aa
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-12-19 14:05:04 -0700

    Submitted transaction.

--- wk2.py
+++ wk2.py
@@ -7,6 +7,8 @@ from algosdk import account, mnemonic
 from algosdk.v2client import algod
 from algosdk.future import transaction
 from algosdk import constants
+import json
+import base64
 
 # creates an account and prints info
 def generate_algorand_keypair():
@@ -36,3 +38,26 @@ def first_transaction_example(private_key, my_address):
 
     # sign transaction
     signed_txn = unsigned_txn.sign(private_key)
+
+    #submit transaction
+    txid = algod_client.send_transaction(signed_txn)
+    print("Successfully sent transaction with txID: {}".format(txid))
+
+    # wait for confirmation 
+    try:
+        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)  
+    except Exception as err:
+        print(err)
+        return
+
+    print("Transaction information: {}".format(
+        json.dumps(confirmed_txn, indent=4)))
+    print("Decoded note: {}".format(base64.b64decode(
+        confirmed_txn["txn"]["txn"]["note"]).decode()))
+    print("Starting Account balance: {} microAlgos".format(account_info.get('amount')) )
+    print("Amount transfered: {} microAlgos".format(amount) )    
+    print("Fee: {} microAlgos".format(params.fee) ) 
+
+
+    account_info = algod_client.account_info(my_address)
+    print("Final Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
