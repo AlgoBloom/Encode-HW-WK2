04fa860d5c644d475d8da47b260482ca05e52347
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-12-19 13:41:24 -0700

    Added code to create account and noted info for generated test account.

---
+++ wk2.py
@@ -0,0 +1,13 @@
+from algosdk import account, mnemonic
+
+def generate_algorand_keypair():
+    private_key, address = account.generate_account()
+    print("My address: {}".format(address))
+    print("My private key: {}".format(private_key))
+    print("My passphrase: {}".format(mnemonic.from_private_key(private_key)))
+
+generate_algorand_keypair()
+
+# My address: BSTDOSCCPYN3AZNRGBX3VO4XBEAH2TKCGKYZPMYIIRNYAFHJZGNASJMOEI
+# My private key: cYGCLpViChb078xonSF43x/IUQvdFlI0jPeD30DZCwIMpjdIQn4bsGWxMG+6u5cJAH1NQjKxl7MIRFuAFOnJmg==
+# My passphrase: comfort anxiety nuclear citizen below airport leisure smooth public major rose worth mother stamp tribe bitter medal cotton wink wealth like wagon aware abandon witness
