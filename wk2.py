37565446191a2d1a8b2103873d32651405de1420
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-12-19 15:18:45 -0700

    Cleaned up code.

--- wk2.py
+++ wk2.py
@@ -79,14 +79,11 @@ two_my_address, two_my_private_key, two_my_passphrase = generate_algorand_keypai
 ### This function funds the new account with a previously funded testnet account
 fund_new_acct("cYGCLpViChb078xonSF43x/IUQvdFlI0jPeD30DZCwIMpjdIQn4bsGWxMG+6u5cJAH1NQjKxl7MIRFuAFOnJmg==", "BSTDOSCCPYN3AZNRGBX3VO4XBEAH2TKCGKYZPMYIIRNYAFHJZGNASJMOEI", two_my_address)
 
-
-############ Test ################################################################################################################################################
+#### ASSET TRANSACTIONS ####
 
 mnemonic1 = "comfort anxiety nuclear citizen below airport leisure smooth public major rose worth mother stamp tribe bitter medal cotton wink wealth like wagon aware abandon witness"
 mnemonic2 = one_my_passphrase
 mnemonic3 = two_my_passphrase
-# never use mnemonics in production code, replace for demo purposes only
-
 # For ease of reference, add account public and private keys to
 # an accounts dict.
 accounts = {}
@@ -97,10 +94,6 @@ for m in [mnemonic1, mnemonic2, mnemonic3]:
     accounts[counter]['sk'] = mnemonic.to_private_key(m)
     counter += 1
 
-# Specify your node address and token. This must be updated.
-# algod_address = ""  # ADD ADDRESS
-# algod_token = ""  # ADD TOKEN
-
 algod_address = "http://localhost:4001"
 algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
@@ -110,9 +103,6 @@ algod_client = algod.AlgodClient(algod_token=algod_token, algod_address=algod_ad
 
 #   Utility function used to print created asset for account and assetid
 def print_created_asset(algodclient, account, assetid):    
-    # note: if you have an indexer instance available it is easier to just use this
-    # response = myindexer.accounts(asset_id = assetid)
-    # then use 'account_info['created-assets'][0] to get info on the created asset
     account_info = algodclient.account_info(account)
     idx = 0;
     for my_account_info in account_info['created-assets']:
@@ -125,9 +115,6 @@ def print_created_asset(algodclient, account, assetid):
 
 #   Utility function used to print asset holding for account and assetid
 def print_asset_holding(algodclient, account, assetid):
-    # note: if you have an indexer instance available it is easier to just use this
-    # response = myindexer.accounts(asset_id = assetid)
-    # then loop thru the accounts returned and match the account you are looking for
     account_info = algodclient.account_info(account)
     idx = 0
     for my_account_info in account_info['assets']:
@@ -142,17 +129,9 @@ print("Account 1 address: {}".format(accounts[1]['pk']))
 print("Account 2 address: {}".format(accounts[2]['pk']))
 print("Account 3 address: {}".format(accounts[3]['pk']))
 
-# your terminal output should look similar to the following
-# Account 1 address: ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ
-# Account 2 address: AK6Q33PDO4RJZQPHEMODC6PUE5AR2UD4FBU6TNEJOU4UR4KC6XL5PWW5K4
-# Account 3 address: IWR4CLLCN2TIVX2QPVVKVR5ER5OZGMWAV5QB2UIPYMPKBPLJZX4C37C4AA
-
 # CREATE ASSET
 # Get network params for transactions before every transaction.
 params = algod_client.suggested_params()
-# comment these two lines if you want to use suggested params
-# params.fee = 1000
-# params.flat_fee = True
 
 # Account 1 creates an asset called latinum and
 # sets Account 2 as the manager, reserve, freeze, and clawback address.
@@ -206,44 +185,12 @@ try:
 except Exception as e:
     print(e)
 
-# terminal output should be similar to below
-# Transaction WVG5HSCU7OIMFHLQGMPJF3NZ56A6FE3DMFUNBUKH73ZUMIU3N3HA confirmed in round 3982822.
-# Waiting for confirmation...
-# Transaction DDDNZWERVG54J32PLCJQENLC5FAFIFYY6ZSYRY25C3J26TJKJ5IA confirmed in round 3982906.
-# Asset ID: 2653870
-# {
-#     "clawback": "AK6Q33PDO4RJZQPHEMODC6PUE5AR2UD4FBU6TNEJOU4UR4KC6XL5PWW5K4",
-#     "creator": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "decimals": 0,
-#     "default-frozen": false,
-#     "freeze": "AK6Q33PDO4RJZQPHEMODC6PUE5AR2UD4FBU6TNEJOU4UR4KC6XL5PWW5K4",
-#     "manager": "AK6Q33PDO4RJZQPHEMODC6PUE5AR2UD4FBU6TNEJOU4UR4KC6XL5PWW5K4",
-#     "metadata-hash": "MTZlZmFhMzkyNGE2ZmQ5ZDNhNDgyNDc5OWE0YWM2NWQ=",
-#     "name": "latinum",
-#     "reserve": "AK6Q33PDO4RJZQPHEMODC6PUE5AR2UD4FBU6TNEJOU4UR4KC6XL5PWW5K4",
-#     "total": 1000,
-#     "unit-name": "LATINUM",
-#     "url": "https://path/to/my/asset/details"
-# }
-# Asset ID: 2653870
-# {
-#     "amount": 1000,
-#     "asset-id": 2653870,
-#     "creator": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "is-frozen": false
-# }
-
-
 # CHANGE MANAGER
 
 # The current manager(Account 2) issues an asset configuration transaction that assigns Account 1 as the new manager.
 # Keep reserve, freeze, and clawback address same as before, i.e. account 2
 params = algod_client.suggested_params()
-# comment these two lines if you want to use suggested params
-# params.fee = 1000
-# params.flat_fee = True
 
-# asset_id = 328952;
 
 txn = AssetConfigTxn(
     sender=accounts[2]['pk'],
@@ -272,32 +219,13 @@ except Exception as err:
     print(err)
 # Check asset info to view change in management. manager should now be account 1
 print_created_asset(algod_client, accounts[1]['pk'], asset_id)
-# terminal output should be similar to...
-# Transaction Y7EYBJNFP7YPGCV7ZD47PMJZHXB2PRT3SZ534M7BZE7G55IMPKUA confirmed in round 3982910.
-# Asset ID: 2653870
-# {
-#     "clawback": "AK6Q33PDO4RJZQPHEMODC6PUE5AR2UD4FBU6TNEJOU4UR4KC6XL5PWW5K4",
-#     "creator": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "decimals": 0,
-#     "default-frozen": false,
-#     "freeze": "AK6Q33PDO4RJZQPHEMODC6PUE5AR2UD4FBU6TNEJOU4UR4KC6XL5PWW5K4",
-#     "manager": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "metadata-hash": "MTZlZmFhMzkyNGE2ZmQ5ZDNhNDgyNDc5OWE0YWM2NWQ=",
-#     "name": "latinum",
-#     "reserve": "AK6Q33PDO4RJZQPHEMODC6PUE5AR2UD4FBU6TNEJOU4UR4KC6XL5PWW5K4",
-#     "total": 1000,
-#     "unit-name": "LATINUM",
-#     "url": "https://path/to/my/asset/details"
-# }
 
 # OPT-IN
 
 # Check if asset_id is in account 3's asset holdings prior
 # to opt-in
 params = algod_client.suggested_params()
-# comment these two lines if you want to use suggested params
-# params.fee = 1000
-# params.flat_fee = True
+
 
 account_info = algod_client.account_info(accounts[3]['pk'])
 holding = None
@@ -334,24 +262,11 @@ if not holding:
     # This should now show a holding with a balance of 0.
     print_asset_holding(algod_client, accounts[3]['pk'], asset_id)
 
-# terminal output should look similar to this...
-
-# Transaction ACYWQVRO6XKQNIHHGH7PDIPKPGURES6YA7OCI654PTR75RKTL4FA confirmed in round 3982915.
-# Asset ID: 2653870
-# {
-#     "amount": 0,
-#     "asset-id": 2653870,
-#     "creator": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "is-frozen": false
-# }
-
 # TRANSFER ASSET
 
 # transfer asset of 10 from account 1 to account 3
 params = algod_client.suggested_params()
-# comment these two lines if you want to use suggested params
-# params.fee = 1000
-# params.flat_fee = True
+
 txn = AssetTransferTxn(
     sender=accounts[1]['pk'],
     sp=params,
@@ -373,22 +288,10 @@ except Exception as err:
 # The balance should now be 10.
 print_asset_holding(algod_client, accounts[3]['pk'], asset_id)
 
-# terminal output should look similar to this...
-# Transaction AYL3FKK6IUWRV2RRCWFBZYO3STX2D74XML6HFWH4EELSDLMLUCCQ confirmed in round 3982920.
-# Asset ID: 2653870
-# {
-#     "amount": 10,
-#     "asset-id": 2653870,
-#     "creator": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "is-frozen": false
-# }
-
 # FREEZE ASSET
 
 params = algod_client.suggested_params()
-# comment these two lines if you want to use suggested params
-# params.fee = 1000
-# params.flat_fee = True
+
 # The freeze address (Account 2) freezes Account 3's latinum holdings.
 
 txn = AssetFreezeTxn(
@@ -412,23 +315,11 @@ except Exception as err:
 # The balance should now be 10 with frozen set to true.
 print_asset_holding(algod_client, accounts[3]['pk'], asset_id)
 
-# Terminal output should look similar to this wih a frozen value of true...
-# Transaction 5NFHUQ4GEQMT4EFPMIIBTHNOX4LS5GQLZRKCKCA2GAUVAS4PAGJQ confirmed in round 3982928.
-# Asset ID: 2653870
-# {
-#     "amount": 10,
-#     "asset-id": 2653870,
-#     "creator": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "is-frozen": true
-# }
-
 # REVOKE ASSET
 
 # The clawback address (Account 2) revokes 10 latinum from Account 3 and places it back with Account 1.
 params = algod_client.suggested_params()
-# comment these two lines if you want to use suggested params
-# params.fee = 1000
-# params.flat_fee = True
+
 
 # Must be signed by the account that is the Asset's clawback address
 txn = AssetTransferTxn(
@@ -459,32 +350,11 @@ print_asset_holding(algod_client, accounts[3]['pk'], asset_id)
 print("Account 1")
 print_asset_holding(algod_client, accounts[1]['pk'], asset_id)
 
-# Terminal output should be similar to...
-# Transaction 4UFNTECSEBAGJT52XLIBM7BQXHBTXUHLZ2U4M4XTZUAVE4VLKURQ confirmed in round 3982932.
-# Account 3
-# Asset ID: 2653870
-# {
-#     "amount": 0,
-#     "asset-id": 2653870,
-#     "creator": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "is-frozen": true
-# }
-# Account 1
-# Asset ID: 2653870
-# {
-#     "amount": 1000,
-#     "asset-id": 2653870,
-#     "creator": "ATTR6RUEHHBHXKUHT4GUOYWNBVDV2GJ5FHUWCSFZLHD55EVKZWOWSM7ABQ",
-#     "is-frozen": false
-# }
 
 # DESTROY ASSET
 # With all assets back in the creator's account,
 # the manager (Account 1) destroys the asset.
 params = algod_client.suggested_params()
-# comment these two lines if you want to use suggested params
-# params.fee = 1000
-# params.flat_fee = True
 
 # Asset destroy transaction
 txn = AssetConfigTxn(
@@ -496,7 +366,6 @@ txn = AssetConfigTxn(
 
 # Sign with secret key of creator
 stxn = txn.sign(accounts[1]['sk'])
-# Send the transaction to the network and retrieve the txid.
 # Send the transaction to the network and retrieve the txid.
 try:
     txid = algod_client.send_transaction(stxn)
@@ -519,9 +388,3 @@ try:
     # asset_info = algod_client.asset_info(asset_id)
 except Exception as e:
     print(e)
-
-# Transaction C7BOB7ZNVIJ477LEAIJYDNXIIFZTY7ETTB3QEV3GWRJ7BGOZMSGA confirmed in round 3983117.
-# Account 3 must do a transaction for an amount of 0,
-# with a close_assets_to to the creator account, to clear it from its accountholdings
-# For Account 1, nothing should print after this if the asset is destroyed on the creator account
-
